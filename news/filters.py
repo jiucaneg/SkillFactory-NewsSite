0f1ae57fbd5a466da2c9c3884fdd84ad3494f69c
Anton Shymko <jiucaneg2012@gmail.com>
2021-09-19 16:57:10 +0300

    add filters

--- news/filters.py
+++ news/filters.py
@@ -1,14 +1,11 @@
-
-from django_filters import FilterSet
+from django_filters import filters, FilterSet
 from .models import Post
 
 
 class PostFilter(FilterSet):
+    title = filters.CharFilter(label='Заголовок', lookup_expr='icontains')
+    date_creation = filters.DateFilter(label='Дата', lookup_expr='gt')
 
     class Meta:
         model = Post
-        fields = {
-            'title': ['icontains'],
-            'date_creation': ['gt'],
-            'author_us': ['in'],
-        }
+        fields = ['title', 'date_creation', 'author_us']
